74db1c98a20d48165298cd8aa1f0e91955000284
Mario L <m9lopez@ucsd.edu>
2022-01-03 17:09:59 -0800

    working??

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 function Home() {
-    return (
-        <div>
-            <h2 className="text-6xl">Welcome</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className='text-6xl'>Welcome</h2>
+      {process.env.REACT_APP_GITHUB_TOKEN}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
